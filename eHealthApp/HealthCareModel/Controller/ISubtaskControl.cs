f4d7c8e484f5f6796707e434d538366eaae24b40
Shortie Monk <andrei.guiu@gmail.com>
2015-10-28 20:24:43 +0100

    v 0.1.1 -basic object model via LINQ classes -CRUD interfaces -see Vuk + Guiu as reference for interfaces

--- eHealthApp/HealthCareModel/Controller/ISubtaskControl.cs
+++ eHealthApp/HealthCareModel/Controller/ISubtaskControl.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace eHealthApp
+namespace HealthCareModel.Controller
 {
-    public class Class1
+    interface ISubtaskControl
     {
     }
 }
