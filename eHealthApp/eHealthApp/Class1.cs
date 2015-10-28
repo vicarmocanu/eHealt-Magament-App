51fb4eaa3f532a7dcecdb123a2dc2b3b336b6b76
Shortie Monk <andrei.guiu@gmail.com>
2015-10-28 19:52:29 +0100

    v 0.1

---
+++ eHealthApp/eHealthApp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eHealthApp
+{
+    public class Class1
+    {
+    }
+}
