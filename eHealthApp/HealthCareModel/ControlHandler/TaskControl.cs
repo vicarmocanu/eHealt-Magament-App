169d9761f6f21a0cd512e5ff4b1dec8e7bace634
Shortie Monk <andrei.guiu@gmail.com>
2015-11-03 20:01:32 +0100

    v 0.1.6 -an example was shown

--- eHealthApp/HealthCareModel/ControlHandler/TaskControl.cs
+++ eHealthApp/HealthCareModel/ControlHandler/TaskControl.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthCareModel.DBHandler;
+using HealthCareModel.Object_Models;
 
 namespace HealthCareModel.Controller
 {
-    class TaskControl
+    public static class TaskControl
     {
+        private static ITaskHandler taskHandler = new TaskHandler();
+
+        public static void createTask(string taskName, bool status, string description)
+        {
+            taskHandler.createTask(taskName, status, description);
+        }
     }
 }
---
+++ eHealthApp/HealthCareModel/DBHandler/TaskHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareModel.DBHandler
+{
+    class TaskHandler : ITaskHandler
+    {
+        public void createTask(string taskName, bool status, string description)
+        {
+            //code to follow
+        }
+    }
+}
