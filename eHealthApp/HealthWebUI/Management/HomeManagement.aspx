4594dfc7c915870e5350043f01cc75dbde8c6dcd
Victor Carmocanu <vicarmocanu@gmail.com>
2015-11-13 07:12:02 +0100

    Added MasterPage, Added BootStrap, Added WCF Services, User Controls Work  -(NO Update) Work, Fixed a few Model&UI Bugs, Added minimal CSS, Root Directory Stable.
    Hakuna Matata!

--- eHealthApp/HealthWebUI/Management/HomeManagement.aspx
+++
@@ -1,5 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Management/HomeManagement.Master" AutoEventWireup="true" CodeBehind="HomeManagement.aspx.cs" Inherits="HealthWebUI.Management.HomeManagement" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-</asp:Content>
--- eHealthApp/HealthWebUI/Management/User/UserHome.aspx.cs
+++ eHealthApp/HealthWebUI/Management/User/UserHome.aspx.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using HealthWebUI.UserServiceReference;
+using HealthCareModel.Object_Models;
 
 namespace HealthWebUI.Management.User
 {
-    public partial class Home : System.Web.UI.Page
+    public partial class UserHome : System.Web.UI.Page
     {
+        private UserServiceReference.UserServiceClient userService = new UserServiceReference.UserServiceClient();
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                RepeaterDataBinding();
+            }
+        }
 
+        protected void UserRepeater_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            switch (e.CommandName.ToString())
+            {
+               case "updateUser":
+                    string userName = Convert.ToString(e.CommandArgument);
+                    User["userName"] = userName; // nu inteleg ce se intampla aici. Merge cu UserControler.
+                    Response.Redirect("~/Management/User/UpdateUser.aspx");
+                    break;
+                   
+                default:
+                    break;
+            }
+        } 
+
+        protected void createButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Management/User/CreateUser.aspx");
+        }
+
+        public void RepeaterDataBinding()
+        {
+            try
+            {
+                this.UserRepeater.DataSource = userService.getUsers();
+                this.UserRepeater.DataBind();
+            }
+            catch (NullReferenceException) { }
         }
     }
 }
